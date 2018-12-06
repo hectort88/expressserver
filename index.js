d99f8ba21c3ab260cfb4faf9fbd0aad994dd392f
Hector Trujillo <hectort88@outlook.com>
2018-12-06 14:56:39 -0500

    Use dotenv

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const app = require("express")();
+const port = process.env.port || 3000;
 
 app.get("/", (request, response) => {
-    response.send("Hello World");
+    response.send({"Message": "Hello World"});
 });
+
+app.listen(port, () => console.log(`Corriendo en el puerto ${port}`));
